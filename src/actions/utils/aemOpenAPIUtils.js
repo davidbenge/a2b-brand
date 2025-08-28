a5f546ffd5e9b966c4a7fe5ab24149df5565c4de
Atul Jadhav <atuljadhav@adobe.com>
2025-08-28 08:25:09 -0400

    adjusting process.env to appconfig driven params

--- src/actions/utils/aemOpenAPIUtils.js
+++ src/actions/utils/aemOpenAPIUtils.js
@@ -10,13 +10,30 @@ const { getJwtToken, getServer2ServerToken } = require('./adobeAuthUtils')
  */
 
 async function fetchAssetFromPreassigned(params,logger){
+    let LOCAL_S2S_SCOPES='["AdobeID","openid","read_organizations","additional_info.projectedProductContext","additional_info.roles","adobeio_api","read_client_secret","manage_client_secrets","event_receiver_api","aem.folders","aem.assets.author"]';
+    let scopesCleaned = JSON.parse(LOCAL_S2S_SCOPES);
+    let scope = scopesCleaned.join(',');
+    let orgs = params.ORG_ID;
+    let clientId = params.S2S_CLIENT_ID;
+    let clientSecret = params.S2S_CLIENT_SECRET;
+    let s2sAuthenticationCredentials = {
+      "client_id": clientId,
+      "client_secret": clientSecret,
+      "orgId": orgs,
+      "scope": scope
+    }
+
+    aemAuthToken = await getServer2ServerToken(s2sAuthenticationCredentials,logger);
+
+    logger.debug("getAemAuth process complete getServer2ServerToken:",aemAuthToken);
+
     let data = JSON.stringify({
       "assetMetadata": {
         "dc:title": params.data.metadata["dc:description"],
         "xmp:Rating": 5,
         "prism:expirationDate": ""
       },
-      "folder": "urn:aaid:aem:3eef3dc2-4141-44f8-8f7f-c931f9f29ea1",
+      "folder": params.DESTINATION_FOLDER_ID,
       "files": [
         {
           "fileName": path.basename(params.data.asset_path),
@@ -29,23 +46,14 @@ async function fetchAssetFromPreassigned(params,logger){
         }
       ]
     });
-    
-    logger.debug("getAemAuth getServer2ServerToken",params)
-    let scopesCleaned = JSON.parse(params.S2S_SCOPES);
-    let scopes = scopesCleaned.join(',');
-    const orgs = params.ORG_ID;
-    const clientId = params.S2S_CLIENT_ID;
-    const clientSecret = params.S2S_CLIENT_SECRET;
-    aemAuthToken = await getServer2ServerToken(clientId,clientSecret,orgs,scopes,logger);
-
     let config = {
       method: 'post',
       maxBodyLength: Infinity,
-      url: 'https://author-'+process.env.AIO_ASSET_SYNCH_TARGET_PROGRAM_ENVIRONMENT+'.adobeaemcloud.com/adobe/assets/import/fromUrl',        
+      url: 'https://author-'+params.AIO_ASSET_SYNCH_TARGET_PROGRAM_ENVIRONMENT+'.adobeaemcloud.com/adobe/assets/import/fromUrl',        
       headers: { 
-        'x-api-key': process.env.AGENCY_BASE_URL, 
-        'authorization': aemAuthToken,
-        'Content-Type': process.env.CONTENT_TYPE
+        'x-api-key': params.S2S_API_KEY, 
+        'authorization': 'Bearer '+aemAuthToken,
+        'Content-Type': params.CONTENT_TYPE
       },
       data : data
     };
